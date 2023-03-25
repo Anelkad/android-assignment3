36e48e190700396919f8e2cca69aaa1ab7d94f0e
Anelkad <an_kadyova@kbtu.kz>
2023-03-25 11:49:14 +0600

    first

---
+++ app/src/main/java/com/example/assignment3/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.assignment3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+    fun shopNow(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
